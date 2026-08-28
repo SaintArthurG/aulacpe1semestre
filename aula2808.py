2b274159f89b4d44b07d5b83ee90315d4bf8458c
saintarthurg <cazooklol@gmail.com>
2026-08-28 10:48:13 -0300

    frases palindromas

---
+++ aula2808.py
@@ -0,0 +1,14 @@
+a=input("Digite uma palavra palindroma: ")
+a=a.replace(" ", "")
+a= a.lower();
+
+if (a == a[::-1]) :
+    print(a);
+else: 
+    print("nao é um palindromo")
+
+
+if (condicao) :
+    1+1
+else :
+    1-1
